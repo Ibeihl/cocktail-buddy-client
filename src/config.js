6b880232165625b0f51943fa646ac25d134c381d
Ian Beihl <ianbeihl@gmail.com>
2018-08-20 20:33:02 -0700

    spaced

--- src/config.js
+++ src/config.js
@@ -3,5 +3,5 @@ require('dotenv').config();
 
 export const API_BASE_URL =
     process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api';
-
+ 
 export const COCKTAIL_KEY = process.env.REACT_APP_COCKTAIL_KEY;
