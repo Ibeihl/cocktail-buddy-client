677bbf0b2b016beb3f9d2c864f6fc565f92a63dc
Ian Beihl <ianbeihl@gmail.com>
2018-10-30 19:55:34 -0700

    set eslint to airbnb

--- src/components/add-drink-btn.js
+++ src/components/add-drink-btn.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import {toggleAddDrink} from '../actions/toggle-add';
-import {connect} from 'react-redux';
-import '../css/add-drink-btn.css'
+import { connect } from 'react-redux';
+import { toggleAddDrink } from '../actions/toggle-add';
+import '../css/add-drink-btn.css';
 
 export class AddDrinkBtn extends React.Component {
-    render(){
-        return (
-            <button className="add-button" onClick={() => this.props.dispatch(toggleAddDrink())}>Add a drink!</button>
-        )
-    }
+  render() {
+    return (
+          <button className="add-button" onClick={() => this.props.dispatch(toggleAddDrink())}>Add a drink!</button>
+    );
+  }
 }
 
 const mapStateToProps = state => ({
-    addDrink: state.drink.addDrink
+  addDrink: state.drink.addDrink,
 });
 
 export default connect(mapStateToProps)(AddDrinkBtn);
--- src/components/add-drink.js
+++ src/components/add-drink.js
@@ -1,28 +1,26 @@
 import React from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import DrinkForm from './drink-form';
 
 export class AddDrink extends React.Component {
-
-    render(){
-        let drinkForm;
-        if(this.props.addDrink === false){
-            drinkForm = '';
-        }
-        else if (this.props.addDrink === true) {
-            drinkForm = <DrinkForm />;
-        }
-        return (
-            <div className="add-drink">
-                { drinkForm }
-                </div>
-        )
+  render() {
+    let drinkForm;
+    if (this.props.addDrink === false) {
+      drinkForm = '';
+    } else if (this.props.addDrink === true) {
+      drinkForm = <DrinkForm />;
     }
+    return (
+          <div className="add-drink">
+              { drinkForm }
+            </div>
+    );
+  }
 }
 
 const mapStateToProps = state => ({
-    addDrink: state.drink.addDrink,
-    currentUser: state.auth.currentUser
+  addDrink: state.drink.addDrink,
+  currentUser: state.auth.currentUser,
 });
 
 export default connect(mapStateToProps)(AddDrink);
